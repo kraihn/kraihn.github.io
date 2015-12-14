1a1ef3d1013daac82cdb66c428a21df39132fbfb
Jared Dickson <code@jareddickson.com>
2015-12-13 22:10:22 -0500

    Rebuilding site Sun Dec 13 22:10:22 EST 2015

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add --all
+
+# Commit changes.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin develop
+git subtree push --prefix=public origin master
